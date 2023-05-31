626071c05009d582f828b0f981ab132b83586ba4
miusarname <oscarm.alvarezg@gmail.com>
2023-05-31 17:22:51 -0500

    feat: :bug: Se recreo casi desde 0
    .

--- CalculadorEintein.php
+++ CalculadorEintein.php
@@ -1,7 +1,20 @@
 <?php
-require "./Scripts/session.php";
+    session_start();
+    if (isset($_POST['numero'])) {
+        if($_POST['numero'] == "c"){
+            $_SESSION['num1'] = null;
+        }else if($_POST['numero'] == "←"){
+            $_SESSION['num1'] = substr($_SESSION['num1'],0, -1);
+            // $_SESSION['num1'] = substr($_SESSION['num1'], 0, strlen($_SESSION['num1']) - 1);
+        }else{
+            if (isset($_SESSION['num1'])) {
+                $_SESSION['num1'] .= $_POST['numero'];
+            } else {
+                $_SESSION['num1'] =  $_POST['numero'];
+            }
+        }
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="sp">
 <head>
@@ -11,26 +24,14 @@ require "./Scripts/session.php";
     <title>Document</title>
 </head>
 <body>
-    <form method="POST" >
-        <input type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>"><br>
+    <form method="POST">
+        <input type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>">
         <button type="submit" name="numero" value="1">1</button>
         <button type="submit" name="numero" value="2">2</button>
         <button type="submit" name="numero" value="3">3</button>
-        <button type="submit" name="numero" value="+">+</button><br>
-        <button type="submit" name="numero" value="4">4</button>
-        <button type="submit" name="numero" value="5">5</button>
-        <button type="submit" name="numero" value="6">6</button>
-        <button type="submit" name="numero" value="-">-</button><br>
-        <button type="submit" name="numero" value="7">7</button>
-        <button type="submit" name="numero" value="8">8</button>
-        <button type="submit" name="numero" value="9">9</button>
-        <button type="submit" name="numero" value="*">*</button><br>
-        <button type="submit" name="numero" value="0">0</button>
-        <button type="submit" name="numero" value="/">/</button>
-        <button type="submit" name="numero" value="c">c</button><br>
+        <button type="submit" name="numero" value="c">c</button>
         <button type="submit" name="numero" value="←">←</button>
-        <input type="submit" value="=">
+        <input type="submit" value="Enviar">
     </form>
-    
 </body>
 </html>
