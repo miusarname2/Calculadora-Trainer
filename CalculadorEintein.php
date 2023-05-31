5802cbac8cd967c793cf4e040fa0fcce002a56d7
miusarname <oscarm.alvarezg@gmail.com>
2023-05-31 17:25:23 -0500

    feat: :sparkles: Se añadieron botones

--- CalculadorEintein.php
+++ CalculadorEintein.php
@@ -25,12 +25,23 @@
 </head>
 <body>
     <form method="POST">
-        <input type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>">
+        <input type="text" name="resultado" value="<?php echo isset($_SESSION['num1']) ? $_SESSION['num1'] :0;?>"><br>
         <button type="submit" name="numero" value="1">1</button>
         <button type="submit" name="numero" value="2">2</button>
         <button type="submit" name="numero" value="3">3</button>
         <button type="submit" name="numero" value="c">c</button>
-        <button type="submit" name="numero" value="←">←</button>
+        <button type="submit" name="numero" value="←">←</button><br>
+        <button type="submit" name="numero" value="4">4</button>
+        <button type="submit" name="numero" value="5">5</button>
+        <button type="submit" name="numero" value="6">6</button>
+        <button type="submit" name="numero" value="7">7</button>
+        <button type="submit" name="numero" value="8">8</button><br>
+        <button type="submit" name="numero" value="9">9</button>
+        <button type="submit" name="numero" value="0">0</button>
+        <button type="submit" name="numero" value="+">+</button>
+        <button type="submit" name="numero" value="-">-</button>
+        <button type="submit" name="numero" value="*">*</button>
+        <button type="submit" name="numero" value="/">/</button>
         <input type="submit" value="Enviar">
     </form>
 </body>
