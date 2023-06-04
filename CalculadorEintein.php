6171c4438fee3871ac74cf24ea81506aa6dc2ca3
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-06-03 19:56:45 -0500

    feat: :zap: Se completo el proyecto con los estilos respectivos

--- CalculadorEintein.php
+++ CalculadorEintein.php
@@ -1,30 +1,33 @@
 <?php
 session_start();
+
 if (isset($_POST['numero'])) {
-    if ($_POST['numero'] == "c") {
+    $numero = $_POST['numero'];
+    
+    if ($numero == "c") {
         $_SESSION['num1'] = null;
         $_SESSION['operador'] = null;
         $_SESSION['num2'] = null;
-    } else if ($_POST['numero'] == "←") {
+    } elseif ($numero == "←") {
         $_SESSION['num1'] = substr($_SESSION['num1'], 0, -1);
-    } else if (is_numeric($_POST['numero'])) {
+    } elseif (is_numeric($numero)) {
         if ($_SESSION['operador'] == null) {
-            $_SESSION['num1'] = isset($_SESSION['num1']) ? $_SESSION['num1'] . $_POST['numero'] : $_POST['numero'];
+            $_SESSION['num1'] .= $numero;
         } else {
-            $_SESSION['num2'] = isset($_SESSION['num2']) ? $_SESSION['num2'] . $_POST['numero'] : $_POST['numero'];
+            $_SESSION['num2'] .= $numero;
         }
     } else {
-        $_SESSION['operador'] = $_POST['numero'];
+        $_SESSION['operador'] = $numero;
     }
 }
 
-if (isset($_SESSION['num1']) && isset($_SESSION['num2']) && isset($_SESSION['operador'])) {
+if (isset($_SESSION['num1'], $_SESSION['num2'], $_SESSION['operador'])) {
     $resultado = calcular($_SESSION['num1'], $_SESSION['num2'], $_SESSION['operador']);
     $_SESSION['num1'] = $resultado;
     $_SESSION['operador'] = null;
     $_SESSION['num2'] = null;
 } else {
-    $resultado = isset($_SESSION['num1']) ? $_SESSION['num1'] : 0;
+    $resultado = $_SESSION['num1'] ?? 0;
 }
 
 function calcular($num1, $num2, $operador)
@@ -42,6 +45,7 @@ function calcular($num1, $num2, $operador)
             return 0;
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -51,28 +55,29 @@ function calcular($num1, $num2, $operador)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Calculadora</title>
 </head>
 
 <body>
     <form method="POST">
         <input type="text" name="resultado" value="<?php echo $resultado; ?>"><br>
-        <button type="submit" name="numero" value="1">1</button>
-        <button type="submit" name="numero" value="2">2</button>
-        <button type="submit" name="numero" value="3">3</button>
-        <button type="submit" name="numero" value="c">c</button>
-        <button type="submit" name="numero" value="←">←</button><br>
-        <button type="submit" name="numero" value="4">4</button>
-        <button type="submit" name="numero" value="5">5</button>
-        <button type="submit" name="numero" value="6">6</button>
-        <button type="submit" name="numero" value="7">7</button>
-        <button type="submit" name="numero" value="8">8</button><br>
-        <button type="submit" name="numero" value="9">9</button>
-        <button type="submit" name="numero" value="0">0</button>
-        <button type="submit" name="numero" value="+">+</button>
-        <button type="submit" name="numero" value="-">-</button>
-        <button type="submit" name="numero" value="*">*</button>
-        <button type="submit" name="numero" value="/">/</button>
+        <button  name="numero" value="1">1</button>
+        <button  name="numero" value="2">2</button>
+        <button  name="numero" value="3">3</button>
+        <button  name="numero" value="c">c</button>
+        <button  name="numero" value="4">4</button>
+        <button  name="numero" value="5">5</button>
+        <button  name="numero" value="6">6</button>
+        <button  name="numero" value="7">7</button>
+        <button  name="numero" value="8">8</button>
+        <button  name="numero" value="9">9</button>
+        <button  name="numero" value="0">0</button>
+        <button  name="numero" value="+">+</button>
+        <button  name="numero" value="-">-</button>
+        <button  name="numero" value="*">*</button>
+        <button  name="numero" value="/">/</button>
+        <button  name="numero" value="←">←</button>
         <input type="submit" value="Enviar">
     </form>
 </body>
